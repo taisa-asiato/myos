227598696b81707a1a43538a169508e0852e6527
taisa-asiato <taisa.aikou@gmail.com>
2018-03-15 01:05:23 +0900

    [ADD] Print test function implemented

--- boot.asm
+++ boot.asm
@@ -21,7 +21,7 @@ BPB_TotSec16	DW	0x0B40	; TotalSectors
 BPB_Media	DB	0xF0	; MediaDescriptor
 BPB_FATSz16	DW	0x0009	; SectorsPerFats
 BPB_SecPerTrk	DW	0x0012	; SectorsPerTrack
-BPB_NUMHeads	DW	0x0002	; NumHeads
+BPB_NumHeads	DW	0x0002	; NumHeads
 BPB_HiddSec	DD	0x00000000	; HiddenSectors
 BPB_ToSec23	DD	0x00000000	; TotalSectors
 
@@ -31,6 +31,93 @@ BP_BootSig	DB	0x29		; BootSignature
 BS_VolID	DD	0x20180310	; VolumeSerialNumber
 BS_VolLab	DB	"MyOS       "	; VolumeLabel
 BS_FilSysType	DB	"FAT12   "	; FileSystemType
+ImageName 	DB	"Hello World", 0x00
+FloppyRead	DB	"Read Sector From Floppy", 0x00
+; ------------------------------------------------------------
+
+;
+; DisplayMessage
+;
+DisplayMessage:
+	PUSH	AX
+	PUSH	BX
+StartDispMsg:
+	LODSB
+	OR	AL, AL
+	JZ	.DONE
+	MOV	AH, 0x0E
+	MOV 	BH, 0x00
+	MOV	BL, 0x07
+	INT	0x10
+	JMP	StartDispMsg
+.DONE:
+	POP	BX
+	POP	AX
+	RET
+
+;
+; For NewLine Function
+;
+NewLine:
+	PUSH 	AX
+	PUSH 	BX
+	MOV	AH, 0x0E
+	MOV	AL, 0x0a	; 改行文字
+	MOV	BH, 0x00
+	MOV	BL, 0x07
+	INT	0x10
+	POP	AX
+	POP	BX
+	RET
+
+;
+; Reset Floppy Drive
+;
+ResetFloppyDrive:
+	MOV	AH, 0x00
+	MOV	DL, 0x00
+	INT 0x13
+	JC	FAILURE
+	HLT
+FAILURE:
+	HLT
+
+;
+; Read Floppy Drive
+;
+ReadSectors:
+	MOV	AH, 0x02
+	MOV	AL, 0x01
+	MOV	CH, 0x01
+	MOV	CL, 0x02
+	MOV	DH, 0x00
+	MOV	DL, 0x00
+	MOV	BX, 0x1000
+	MOV	ES, BX
+	MOV	BX, 0x0000
+	INT 0x13
+
+;
+; LBA2CHS
+;
+LBA2CHS:
+	XOR	DX, DX			; Initailze DX
+	DIV	WORD [BPB_SecPerTrk]	; calculate
+	INC	DL
+	MOV	BYTE [physicalSector], DL
+	XOR	DX, DX
+	DIV	WORD [BPB_NumHeads]
+	MOV 	BYTE [physicalHead], DL
+	MOV	BYTE [physicalTrack], AL
+
+	MOV	SI, FloppyRead
+	CALL	DisplayMessage
+
+	RET
+
+physicalSector	DB	0x00
+physicalHead	DB	0x00
+physicalTrack	DB	0x00
 
 ;
 ; BOOT
@@ -52,6 +139,12 @@ BOOT:
 	MOV	SS, AX
 	MOV	SP, 0xFFFC
 
+
+	MOV	SI, ImageName
+	CALL	DisplayMessage
+	CALL 	NewLine
+
+	CALL	LBA2CHS
 	HLT
 
 TIMES 510 - ( $ - $$ ) DB 0
